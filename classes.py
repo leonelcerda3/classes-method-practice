505aab6e691c3c6d82609700cc55e9c965b4d747
Leonel Cerda <leonelcerda@Leonels-MBP.home>
2025-07-03 09:53:07 -0700

    Add classes.py with Dog class example

--- classes.py
+++ classes.py
@@ -1,8 +1,20 @@
 class Dog:
+
     def __init__(self, name, age):
         self.name = name
         self.age = age 
 
+    
+    def is_new_dog(self):
+        return new_dog_name
+
+    
+
 buddy = Dog("Buddy", 3)
+
 print(buddy.name)
 print(buddy.age)
+
+new_dog_name = input("What should the new dog name be: ")
+
+print(buddy.is_new_dog())
